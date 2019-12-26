6a1f61763a026319a72ff3772adcc36955436459
Ju Lim <julim@redhat.com>
2019-12-26 14:10:46 -0500

    Added more files

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-2_ST5FyxCsR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2r118kd5npamva0n6k66k7ekv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://ujz75nm0r0.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
