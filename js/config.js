0daf6ff0581d2109c2d946d00f5e132650b798b3
Ju Lim <julim@redhat.com>
2019-12-26 14:21:36 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://ujz75nm0r0.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://ujz75nm0r0.execute-api.ap-southeast-2.amazonaws.com/prod',
     }
 };
