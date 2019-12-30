56e4cf8ea91b97ed0ef34f94b07902cc41f37f5d
Ju Lim <julim@redhat.com>
2019-12-30 09:08:16 -0500

    Update esri-map.js

--- js/esri-map.js
+++ js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [-71.09, 42.33],
+            center: [-71.15, 42.37],
             container: 'map',
             map: map,
             zoom: 12
