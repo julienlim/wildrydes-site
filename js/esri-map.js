06bbf286d13f0b0c086d87f8182ea599e15741f4
Ju Lim <julim@redhat.com>
2019-12-30 09:01:41 -0500

    update esri-map.js
    change locale to NYC

--- js/esri-map.js
+++ js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [71.057083, 42.361145],
+            center: [~74.00, 40.71],
             container: 'map',
             map: map,
             zoom: 12
